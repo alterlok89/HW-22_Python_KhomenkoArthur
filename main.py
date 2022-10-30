115562f57bfd67596c66897da0f53e3fe0089b37
alterlo89 <alterlok89@gmail.com>
2022-10-30 14:08:36 +0300

    машина состояния (дополненил тесты)

--- main.py
+++ main.py
@@ -24,9 +24,12 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 users = {}
+callback_word_test = {}
+callback_idiom_test = {}
 
-@dp.message_handler(content_types='contact')
+@dp.message_handler(content_types='contact', state=MyStates.STATES_3)
 async def ph(message: types.Message):
+
     print('--')
     print(message)
     print(message.contact.phone_number)
@@ -35,11 +38,14 @@ async def ph(message: types.Message):
 
 @dp.message_handler(state=MyStates.STATES_0)
 async def word_test(message: types.Message):
+
     state = dp.current_state(user=message.from_user.id)
     # chat_id = message.from_user.id
     i = 0
     count = 0
+
     if message.text == '/cancel':
+
         await message.answer('Закончить тест')
         await state.set_state('*')
     else:
@@ -54,8 +60,11 @@ async def word_test(message: types.Message):
 
 @dp.message_handler(state=MyStates.STATES_1)
 async def idiom_test(message: types.Message):
+
     state = dp.current_state(user=message.from_user.id)
+
     if message.text == '/cancel':
+
         await message.answer('Закончить тест')
         await state.set_state('*')
     else:
@@ -66,11 +75,12 @@ async def idiom_test(message: types.Message):
         list_translate_idiom = [i[3] for i in list_idiom]
         keyboard = kb.inline_keyboard_idiom(list_translate_idiom, num,)
         await message.answer(idiom, reply_markup=keyboard,)
-        print(message.answer)
+        # print(message.answer)
 
 
 @dp.message_handler(state=MyStates.STATES_2)
 async def translate(message: types.Message):
+
     state = dp.current_state(user=message.from_user.id)
     chat_id = message.from_user.id
 
@@ -94,15 +104,17 @@ async def translate(message: types.Message):
 
         await message.answer('Для остановки переводчика введите /cancel')
 
-# @dp.message_handler(state=MyStates.STATES_3)
-# async def user_data(message: types.Message):
-#     print('--')
-#     print(message)
-#     print(message.contact.phone_number)
-#     print(message.from_id)
+@dp.message_handler(state=MyStates.STATES_3)
+async def user_data(message: types.Message):
+
+    print('--')
+    print(message)
+    print(message.contact.phone_number)
+    print(message.from_id)
 
 @dp.message_handler(state='*')
 async def echo(message: types.Message):
+
     chat_id = message.from_user.id
     # print(message)
     user = {
@@ -117,6 +129,7 @@ async def echo(message: types.Message):
     users.update({message.from_user.id: {message.from_user.first_name: message.from_user.username}})
     # print(len(db.get_user(message.from_user.id)))
     if len(db.get_user(message.from_user.id)) == 0:
+
         db.add_item(table='Users', data=user)
 
     users.update({message.from_user.id: message.from_user.first_name})
@@ -131,26 +144,29 @@ async def echo(message: types.Message):
     #        f'Приветствую!!!'
 
     if message.text == '/start' or message.text == 'Начало работы' or message.text == 'Главное меню':
+
         keyboard = kb.keyboard_menu
         await message.answer(message.text, reply_markup=keyboard)
 
     elif message.text == '/word_test' or message.text == 'Тест на знание слов':
 
+        callback_word_test.update({message.from_user.id: []})
         state = dp.current_state(user=chat_id)
         await message.answer(
                 'Вам предложат значение слова на русском языке, '
                 'а так же 6 вариантов ответов на них.\n'
-                '/cancel - для остановки теста')
+                'В тесте 10 вопросов')
         await state.set_state(MyStates.all()[0])
-        print(message)
+        # print(message)
         await word_test(message)
 
     elif message.text == '/idiom_test' or message.text == 'Тест на знание идиом':
 
+        callback_idiom_test.update({message.from_user.id: []})
         state = dp.current_state(user=chat_id)
         await message.answer('Вам предложат английскую идиому, '
                              'а так же 4 варианта их перевода.\n'
-                             '/cancel - для остановки теста')
+                             'В тесте 6 вопросов')
         await state.set_state(MyStates.all()[1])
         await idiom_test(message)
 
@@ -165,18 +181,21 @@ async def echo(message: types.Message):
         await message.answer(text)
 
     elif message.text == 'Обновить данные профиля':
-        # state = dp.current_state(user=chat_id)
+
+        state = dp.current_state(user=chat_id)
         keyboard = kb.get_kbrd()
-        # await state.set_state(MyStates.all()[3])
+        await state.set_state(MyStates.all()[3])
         await message.answer(message.text, reply_markup=keyboard)
 
 
     elif message.text == 'Аудио урок':
+
         keyboard = kb.get_audio_kbrd()
         await message.answer('Выберите урок, который хотите прослушать', reply_markup=keyboard)
 
     lesson = message.text
     if lesson.find('Урок') != -1:
+
         dict = db.get_all_item(table='Lessons')
         mes = message.text
         i = int(mes.replace('Урок ', ''))
@@ -192,9 +211,9 @@ async def echo(message: types.Message):
         await message.answer_audio(audio_url)
 
     elif message.text == '/translate' or message.text == 'Переводчик':
+
         state = dp.current_state(user=chat_id)
-        await message.answer('Введите слово или фразу для перевода'
-                             '/cancel - для остановки теста')
+        await message.answer('Введите слово или фразу для перевода')
         await state.set_state(MyStates.all()[2])
 
 @dp.callback_query_handler(state=MyStates.STATES_0)
@@ -202,46 +221,78 @@ async def call_word(callback_q: types.CallbackQuery, ):
 
     state = dp.current_state(user=callback_q.from_user.id)
     # print(callback_q.message)
-    count = 0
-    num = 0
+    callback_word_test[callback_q.from_user.id].append(callback_q.data)
+    # print(len(callback_word_test[callback_q.from_user.id]))
+    # print(callback_word_test)
+    # количество вопросов
+    num = 10
     await bot.answer_callback_query(callback_q.id)
     await bot.send_message(chat_id=callback_q.from_user.id, text=callback_q.data)
+
     if callback_q.data == 'Не верно!':
+
         all_button_callback = callback_q.message.reply_markup
         for i in all_button_callback['inline_keyboard']:
             if i[0]['callback_data'] == 'Верно!':
                 en_word = i[0]['text']
-                print(en_word)
-        print('Верный ответ - ', en_word, end='\n')
+                # print(en_word)
+        # print('Верный ответ - ', en_word, end='\n')
         callback_text = f'Верный ответ - {en_word}'
         await bot.send_message(chat_id=callback_q.from_user.id, text=callback_text)
-        # num += 1
-    print(callback_q.message.message_id)
-    await bot.send_message(chat_id=callback_q.from_user.id, text='/cancel - для остановки теста')
-    await word_test(callback_q.message)
+
+    if len(callback_word_test[callback_q.from_user.id]) == num:
+
+        correct_answer = [i for i in callback_word_test[callback_q.from_user.id] if i == 'Верно!']
+        correct_answer_text = f'Вы сделали в тесте {len(correct_answer)} ' \
+                              f'правильных ответов из {len(callback_word_test[callback_q.from_user.id])}'
+
+        await bot.send_message(chat_id=callback_q.from_user.id, text=correct_answer_text)
+        callback_word_test[callback_q.from_user.id].clear()
+        await state.set_state('*')
+    else:
+        await word_test(callback_q.message)
 
 
 @dp.callback_query_handler(state=MyStates.STATES_1)
 async def call_idiom(callback_q: types.CallbackQuery, ):
+
     # print(' - ', callback_q, end='\n')
     # print(' - ', callback_q.data, end='\n')
+    state = dp.current_state(user=callback_q.from_user.id)
+    # print(callback_q.message)
+    callback_idiom_test[callback_q.from_user.id].append(callback_q.data)
+    # количество вопросов
+    num = 6
     await bot.answer_callback_query(callback_q.id)
     await bot.send_message(chat_id=callback_q.from_user.id, text=callback_q.data)
+
     if callback_q.data == 'Не верно!':
         all_button_callback = callback_q.message.reply_markup
-        print(all_button_callback['inline_keyboard'])
+        # print(all_button_callback['inline_keyboard'])
         for i in all_button_callback['inline_keyboard']:
             if i[0]['callback_data'] == 'Верно!':
                 ru_text = i[0]['text']
-                print(ru_text)
-        print('Верный ответ - ', ru_text, end='\n')
+                # print(ru_text)
+        # print('Верный ответ - ', ru_text, end='\n')
         callback_text = f'Верный ответ - {ru_text}'
         await bot.send_message(chat_id=callback_q.from_user.id, text=callback_text)
-    await bot.send_message(chat_id=callback_q.from_user.id, text='/cancel - для остановки теста')
-    await word_test(callback_q.message)
+    # await bot.send_message(chat_id=callback_q.from_user.id, text='/cancel - для остановки теста')
+
+    if len(callback_idiom_test[callback_q.from_user.id]) == num:
+
+        correct_answer = [i for i in callback_idiom_test[callback_q.from_user.id] if i == 'Верно!']
+        correct_answer_text = f'Вы сделали в тесте {len(correct_answer)} ' \
+                              f'правильных ответов из {len(callback_idiom_test[callback_q.from_user.id])}'
+
+        await bot.send_message(chat_id=callback_q.from_user.id, text=correct_answer_text)
+        callback_idiom_test[callback_q.from_user.id].clear()
+        await state.set_state('*')
+    else:
+        await idiom_test(callback_q.message)
 
 @dp.callback_query_handler()
 async def call_echo(callback_q: types.CallbackQuery, ):
+
     # print(' - ', callback_q, end='\n')
     # print(' - ', callback_q.data, end='\n')
     await bot.answer_callback_query(callback_q.id)
--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 import requests
 import pyglet
+import os
 
-song = pyglet.media.load('https://dl.dropboxusercontent.com/s/d1pu3sxgyxp48bx/russian_english_001.mp3')
-song.play()
-pyglet.app.run()
+from aiogram import Bot, Dispatcher, executor, types
+from playsound import playsound
+
+import database
+import keyboards
+import keyboards as kb
+import random
+
 
 
 import database, random
@@ -43,7 +49,7 @@ import database, random
 # print(f'Правильных ответов - {count}')
 
 
-# db = database.DataBase()
+db = database.DataBase()
 # dict = db.get_all_item(table='Lessons')
 # print(dict)
 # print(dict[0][3])
@@ -66,4 +72,102 @@ import database, random
 #
 #     bot.send_audio(message.from_user.id, audio)
 
+# audio_path = os.path.expanduser('~/Downloads/muzmo_ru_Metallica_-_Metalica_Fuel_12710830.mp3')
+# url_audio = 'https://dl.dropboxusercontent.com/s/d1pu3sxgyxp48bx/russian_english_001.mp3'
 
+# class State:
+#     states = [
+#         'основное меню',
+#         'ввод х',
+#         'ввод оператора',
+#         'ввод y',
+#     ]
+#     data = {
+#         'x': 'X',
+#         'y': 'Y',
+#         'op': 'operator',
+#     }
+#     state = ''
+#
+#     def __init__(self):
+#         self.state = self.states[0]
+#
+#     def __str__(self):
+#         return f'\n***\n{self.data}\n{self.state}\n***\n'
+#
+#
+# # s = State()
+# # print(State().__str__())
+#
+# dict = db.get_all_item(table='Idioms')
+# idiom = random.sample(dict, 1)
+# text = f'Idiom: {idiom[0][1]}\n' \
+#            f'Synonym: {idiom[0][2]}\n' \
+#            f'Translate: {idiom[0][3]}\n' \
+#        f'Example: {idiom[0][4]}'
+# print(idiom)
+# print(text)
+
+
+# import translators as ts
+# import re
+#
+#
+# wyw_text = 'Привет! Меня зовут Артур, я из Харькова'
+# wyw = 'Hello! My name is Arthur, I\'m from Kharkov'
+#
+#
+# if bool(re.search('[а-яА-Я]', wyw_text)) == False:
+#     print('Перевод с английского')
+#     translate = ts.google(wyw_text, to_language='ru')
+#     print(translate)
+# else:
+#     print('Перевод с руского')
+#     translate = ts.google(wyw_text, to_language='en')
+#     print(translate)
+
+
+
+# print(ts.google(wyw_text)) # default: from_language='auto', to_language='en'
+# print(ts.google(wyw, to_language='ru')) # default: from_language='auto', to_language='en'
+# # output language_map
+# # print(ts._google.language_map)
+
+
+# a = {
+#     "id": "2495673303162143227",
+#     "from": {"id": 581069221, "is_bot": "false", "first_name": "Артур", "username": "alterlok", "language_code": "ru"},
+#     "message": {
+#         "message_id": 1126,
+#         "from": {"id": 5702489430, "is_bot": "true", "first_name": "Learn_english_Bot", "username": "Py21_Arthur_test_Bot"},
+#         "chat": {"id": 581069221, "first_name": "Артур", "username": "alterlok", "type": "private"},
+#         "date": 1667062322, "text": "вилы, камертон, взбрасывать вилами",
+#         "reply_markup": {"inline_keyboard":
+#                              [[{"text": "caught", "callback_data": "Не верно!"}],
+#                               [{"text": "tress", "callback_data": "Не верно!"}],
+#                               [{"text": "bijugate", "callback_data": "Не верно!"}],
+#                               [{"text": "pitchfork", "callback_data": "Верно!"}],
+#                               [{"text": "oddish", "callback_data": "Не верно!"}],
+#                               [{"text": "siphon", "callback_data": "Не верно!"}]]
+#                          }
+#                 },
+#     "chat_instance": "4020572205321823457", "data": "Не верно!"}
+# b = a['message']['reply_markup']['inline_keyboard']
+# print(a['message']['reply_markup']['inline_keyboard'])
+# # print(a.get('reply_markup'))
+# for i in b:
+#     if i[0]['callback_data'] == 'Верно!':
+#         print(i[0]['text'])
+
+callback_word_test = {}
+
+
+callback_word_test.update({'message.from_user.id': []})
+print(callback_word_test)
+a = 'Верно!'
+callback_word_test['message.from_user.id'].append(a)
+callback_word_test['message.from_user.id'].append(a)
+callback_word_test['message.from_user.id'].append(a)
+callback_word_test['message.from_user.id'].append(a)
+
+print(callback_word_test)
